7d9b252dad263b5d46e47fc85a32b721ca8d8a5d
Jarl Larsson <jarl.larsson@gmail.com>
2013-12-27 16:36:08 +0100

    Setting up 3d environment in cinder and playing around with the lib

--- src/proto/main.cpp
+++ src/proto/main.cpp
@@ -2,6 +2,10 @@
 //#include "CinderApp.h"
 
 #include "cinder/app/AppBasic.h"
+#include "cinder/ImageIo.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Camera.h"
+#include "cinder/params/Params.h"
 #include <list>
 
 using namespace ci;
@@ -13,10 +17,22 @@ class CinderApp : public AppBasic {
   public:
 	void mouseDrag( MouseEvent event );
 	void keyDown( KeyEvent event );
+	void setup();
+	void update();
 	void draw();
 
 	// This will maintain a list of points which we will draw line segments between
 	list<Vec2f>		mPoints;
+	gl::Texture		myImage;
+	// camera
+	CameraPersp		mCam;
+	Vec3f			mCamLookat;
+	Vec3f			mCamPos;
+	Vec3f			mCamUp;
+	Quatf			mCamRot;
+
+	// Input
+	params::InterfaceGl mDebugInterface;
 };
 
 void CinderApp::mouseDrag( MouseEvent event )
@@ -30,9 +46,30 @@ void CinderApp::keyDown( KeyEvent event )
 		setFullScreen( ! isFullScreen() );
 }
 
+void CinderApp::setup()
+{
+	mCamPos = Vec3f( 0.0f, 0.0f, 500.0f );
+	mCamLookat = Vec3f::zero();
+	mCamUp = Vec3f::yAxis();
+	myImage = gl::Texture( loadImage( loadAsset( "img.jpg" ) ) );
+
+	// Debug interface
+	mDebugInterface = params::InterfaceGl( "Gait", Vec2i( 225, 200 ) );
+	mDebugInterface.addParam( "Camera angle", &mDebugInterface );
+}
+
+void CinderApp::update()
+{
+	mCam.setPerspective( 60.0f, getWindowAspectRatio(), 5.0f, 3000.0f );
+	mCam.lookAt( mCamPos, mCamLookat, mCamUp );
+	gl::setMatrices( mCam );
+}
+
 void CinderApp::draw()
 {
 	gl::clear( Color( 0.1f, 0.1f, 0.15f ) );
+
+	gl::draw( myImage, getWindowBounds() );
 
 	gl::color( 1.0f, 0.5f, 0.25f );	
 	gl::begin( GL_LINE_STRIP );
